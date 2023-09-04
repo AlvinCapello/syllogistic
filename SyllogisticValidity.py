79de80db4dcba117b1ae84d860cf67bfcfcbc445
AlvinCapello <alvincapello@gmail.com>
2023-09-04 14:56:24 -0500

    Added more functions and further refined the logic for is_valid()

--- SyllogisticValidity.py
+++ SyllogisticValidity.py
@@ -34,6 +34,7 @@ def is_undistributed(p):
         return None
     elif p[1] == "o":
         return p[0]
+
 def is_negative(p):
     if p[1] == "e" or p[1] == "o":
         return True
@@ -49,24 +50,93 @@ def is_positive(p):
 def arg_structure(p,q,r):
     prop_list = []
     for prop in (p,q,r):
-        arg_list.append(prop)
+        prop_list.append(prop)
     return prop_list
 
+def common_term(p,q):
+    middle = ""
+    for i in p:
+        if is_copula(i):
+            pass
+        elif i in q:
+            middle = i
+            break
+    return middle
+
+def is_copula(p):
+    if p == "a" or p == "e" or p == "i" or p == "o":
+        return True
+    else:
+        return False
+
+def term_counter(p,q,r):
+    standard_form = arg_structure(p,q,r)
+    terms = set()
+    for prop in standard_form:
+        for letter in prop:
+            if is_copula(letter):
+                prop.replace(letter,"")
+            else:
+                terms.add(letter)
+    return set(terms)
+
+def distributed_term_finder(p,q,r):
+    terms = set()
+    for prop in (p,q,r):
+        if is_distributed(prop) == None:
+            pass
+        if len(is_distributed(prop)) > 1:
+            for letter in is_distributed(prop):
+                terms.add(letter)
+        else:
+            terms.add(is_distributed(prop))
+    return terms.remove(None)
+
 def is_valid(p,q,r):
-    syllogism = arg_structure[p,q,r]
-    premises = [p,q]
-    total_terms = {p,q,r}
-    distributed_terms = {is_distributed(p), is_distributed(q), is_distributed(r)}
-    undistributed_terms = {is_undistributed(p), is_undistributed(q), is_undistributed(r)}
+    total_terms = term_counter(p,q,r)
+    distributed_terms = distributed_term_finder(p,q,r)
     major_term = r[2]
     minor_term = r[0]
-    middle_term = p.intersection(q)
+    middle_term = common_term(p,q)
+    print(middle_term)
+    print(distributed_terms)
 
-    if middle_term in undistributed_terms:
-        print("Invalid: Your argument commits the Fallacy of Undistributed Middle")
+    if middle_term not in distributed_terms:
+        print("Invalid: Fallacy of Undistributed Middle")
+        return False
     elif is_negative(p) and is_negative(q):
-        print("Invalid: Your argument commits the Fallacy of Two Negative Premises")
+        print("Invalid: Fallacy of Two Negative Premises")
+        return False
     elif len(total_terms) > 3:
-        print("Invalid: Your argument commits the Fallacy of Four Terms")
-    elif major_term in [undistributed_terms,]
+        print("Invalid: Fallacy of Four Terms")
+        return False
+    elif major_term in is_distributed(r):
+        if major_term in is_undistributed(p) and major_term in is_undistributed(q):
+            print("Invalid: Fallacy of Illicit Major")
+            return False
+    elif minor_term in is_distributed(r):
+        if minor_term in is_undistributed(p) and minor_term in is_undistributed(q):
+            print("Invalid: Fallacy of Illicit Minor")
+            return False
+        else:
+            pass
+    elif is_negative(p) or is_negative(q):
+        if is_positive(r):
+            print("Invalid: Negative Premise with Positive Conclusion")
+            return False
+        else:
+            pass
+    elif is_negative(r):
+        if is_positive(p) and is_positive(q):
+            print("Invalid: Positive Premises with Negative Conclusion")
+            return False
+    else:
+        print("Valid")
+        return True
+
+
+is_valid("QiR", "PiQ", "PaR")
+
+distributed_term_finder("QiR", "PiQ", "PaR")
+
 
