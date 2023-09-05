d1869dfc51f2e15fa6dc7dc1283f7a2634bb2588
AlvinCapello <alvincapello@gmail.com>
2023-09-04 19:57:11 -0500

    Finished up the logic for this code.

--- SyllogisticValidity.py
+++ SyllogisticValidity.py
@@ -21,13 +21,13 @@ def is_distributed(p):
     elif p[1] == "a":
         return p[0]
     elif p[1] == "e":
-        return p[0],p[2]
+        return [p[0],p[2]]
     elif p[1] == "o":
         return p[2]
 
 def is_undistributed(p):
     if p[1] == "i":
-        return p[0],p[2]
+        return [p[0],p[2]]
     elif p[1] == "a":
         return p[2]
     elif p[1] == "e":
@@ -81,27 +81,48 @@ def term_counter(p,q,r):
     return set(terms)
 
 def distributed_term_finder(p,q,r):
-    terms = set()
+    terms = {"Premise_1":[],"Premise_2":[],"Conclusion":[]}
     for prop in (p,q,r):
-        if is_distributed(prop) == None:
-            pass
-        if len(is_distributed(prop)) > 1:
-            for letter in is_distributed(prop):
-                terms.add(letter)
-        else:
-            terms.add(is_distributed(prop))
-    return terms.remove(None)
+        if is_distributed(prop) is not None:
+            if prop == p:
+                terms["Premise_1"] = is_distributed(p)
+            elif prop == q:
+                terms["Premise_2"] = is_distributed(q)
+            else:
+                terms["Conclusion"] = is_distributed(r)
+    return terms
+
+def undistributed_term_finder(p,q,r):
+    terms = {"Premise_1":[],"Premise_2":[],"Conclusion":[]}
+    for prop in (p,q,r):
+        if is_undistributed(prop) is not None:
+            if prop == p:
+                terms["Premise_1"] = is_undistributed(p)
+            elif prop == q:
+                terms["Premise_2"] = is_undistributed(q)
+            else:
+                terms["Conclusion"] = is_undistributed(r)
+    return terms
+
+def both_premises_undistributed(dictionary,search_string):
+    if search_string in dictionary["Premise_1"] and search_string in dictionary["Premise_2"]:
+        return True
+    return False
+
+def either_premises_undistributed(dictionary,search_string):
+    if search_string in dictionary["Premise_1"] or search_string in dictionary["Premise_2"]:
+        return True
+    return False
 
 def is_valid(p,q,r):
     total_terms = term_counter(p,q,r)
     distributed_terms = distributed_term_finder(p,q,r)
+    undistributed_terms = undistributed_term_finder(p,q,r)
     major_term = r[2]
     minor_term = r[0]
     middle_term = common_term(p,q)
-    print(middle_term)
-    print(distributed_terms)
-
-    if middle_term not in distributed_terms:
+  
+    if both_premises_undistributed(undistributed_terms,middle_term):
         print("Invalid: Fallacy of Undistributed Middle")
         return False
     elif is_negative(p) and is_negative(q):
@@ -110,22 +131,20 @@ def is_valid(p,q,r):
     elif len(total_terms) > 3:
         print("Invalid: Fallacy of Four Terms")
         return False
-    elif major_term in is_distributed(r):
-        if major_term in is_undistributed(p) and major_term in is_undistributed(q):
+    elif is_distributed(r) is not None and major_term in is_distributed(r):
+        #if is_undistributed(p) is not None and is_undistributed(q) is not None:
+        if either_premises_undistributed(undistributed_terms,major_term):
             print("Invalid: Fallacy of Illicit Major")
             return False
-    elif minor_term in is_distributed(r):
-        if minor_term in is_undistributed(p) and minor_term in is_undistributed(q):
+    elif is_distributed(r) is not None and minor_term in is_distributed(r):
+        #if is_undistributed(p) is not None and is_undistributed(q) is not None:
+        if either_premises_undistributed(undistributed_terms,minor_term):
             print("Invalid: Fallacy of Illicit Minor")
             return False
-        else:
-            pass
     elif is_negative(p) or is_negative(q):
         if is_positive(r):
             print("Invalid: Negative Premise with Positive Conclusion")
             return False
-        else:
-            pass
     elif is_negative(r):
         if is_positive(p) and is_positive(q):
             print("Invalid: Positive Premises with Negative Conclusion")
@@ -135,8 +154,6 @@ def is_valid(p,q,r):
         return True
 
 
-is_valid("QiR", "PiQ", "PaR")
-
-distributed_term_finder("QiR", "PiQ", "PaR")
+is_valid("QiR", "PaQ", "PiR")
 
 
